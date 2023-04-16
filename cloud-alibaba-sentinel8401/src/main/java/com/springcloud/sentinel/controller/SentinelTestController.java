416b8e0c23bac2081e6ad45e720ed80a83125ca0
cxylbx <17615911016@163.com>
2023-04-17 07:25:34 +0800

    代码提交

--- cloud-alibaba-sentinel8401/src/main/java/com/springcloud/sentinel/controller/SentinelTestController.java
+++ cloud-alibaba-sentinel8401/src/main/java/com/springcloud/sentinel/controller/SentinelTestController.java
@@ -1,6 +1,9 @@
 package com.springcloud.sentinel.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,11 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class SentinelTestController {
 
     @GetMapping("/test01")
-    public String test01(){
+    @SentinelResource(value = "test01", blockHandler = "exception")
+    public String test01(@RequestParam(value = "param", required = false) String param){
         return "test01";
     }
 
+    public String exception(String param, BlockException e) {
+        return "错误代码";
+    }
+
     @GetMapping("/test02")
+    @SentinelResource(value = "test02")
     public String test02(){
         return "test02";
     }
